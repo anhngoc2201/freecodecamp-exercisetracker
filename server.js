9b84897075d9de1706e66260f72729d7831387a9
Glitch (wealthy-clave) <none>
2018-09-25 00:18:10 +0000

    💄❄️ Checkpoint ./views/layout.pug:1841575/2323 ./models/user.js:1841575/280 ./old-server.js:1841575/3541 ./server.js:1841575/1565 ./views/index.pug:1841575/941

--- server.js
+++ server.js
@@ -4,16 +4,33 @@
 // init project
 var express = require('express');
 var app = express();
-
+var bodyParser = require('body-parser')
+var mongoose = require('mongoose');
+var autoIncrement = require('mongoose-auto-increment');
+// we've started you off with Express, 
+// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+mongoose.connect(`mongodb://`+ process.env.USERNAME + ':' + process.env.PASSWORD +`@` +process.env.MONGO_URL,{ useNewUrlParser: true })
+       .then(() => {
+         console.log('Database connection successful')
+       })
+       .catch(err => {
+         console.error('Database connection error: ' + err)
+       })
+autoIncrement.initialize(mongoose.connection);
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
+app.set('view engine', 'pug')
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+  response.render(__dirname + '/views/index',{title:"Shorten URL",message:"Enter url"});
 });
 
 // listen for requests :)
