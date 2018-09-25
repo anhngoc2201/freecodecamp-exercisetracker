c94a58471a5a14cadcd9295cb179908e8d40fd11
Glitch (wealthy-clave) <none>
2018-09-25 03:03:16 +0000

    🍣🏉 Checkpoint ./server.js:1841575/10111 ./models/exercise.js:1841575/10 ./views/index.pug:1841575/2

--- models/exercise.js
+++ models/exercise.js
@@ -2,7 +2,7 @@ let mongoose = require('mongoose');
 let autoIncrement = require('mongoose-auto-increment');
 const Schema = mongoose.Schema;
 let exerciseSchema = new mongoose.Schema({
-  user : { type: Schema.Types.ObjectId, ref: 'Author' },
+  userId : { type: Schema.Types.ObjectId, ref: 'Author' },
   date: {type: Date},
   duration: {type: Number},
   description: {type: String}
--- server.js
+++ server.js
@@ -9,13 +9,15 @@ var mongoose = require('mongoose');
 var autoIncrement = require('mongoose-auto-increment');
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
-mongoose.connect(`mongodb://`+ process.env.USERNAME + ':' + process.env.PASSWORD +`@` +process.env.MONGO_URL,{ useNewUrlParser: true })
-       .then(() => {
-         console.log('Database connection successful')
-       })
-       .catch(err => {
-         console.error('Database connection error: ' + err)
-       })
+mongoose.connect(`mongodb://` + process.env.USERNAME + ':' + process.env.PASSWORD + `@` + process.env.MONGO_URL, {
+        useNewUrlParser: true
+    })
+    .then(() => {
+        console.log('Database connection successful')
+    })
+    .catch(err => {
+        console.error('Database connection error: ' + err)
+    })
 autoIncrement.initialize(mongoose.connection);
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
@@ -24,17 +26,19 @@ autoIncrement.initialize(mongoose.connection);
 app.use(express.static('public'));
 app.set('view engine', 'pug')
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
 // parse application/json
 app.use(bodyParser.json())
 
 
 function isValidDate(dateString) {
-  var regEx = /^\d{4}-\d{2}-\d{2}$/;
-  if(!dateString.match(regEx)) return false;  // Invalid format
-  var d = new Date(dateString);
-  if(Number.isNaN(d.getTime())) return false; // Invalid date
-  return d.toISOString().slice(0,10) === dateString;
+    var regEx = /^\d{4}-\d{2}-\d{2}$/;
+    if (!dateString.match(regEx)) return false; // Invalid format
+    var d = new Date(dateString);
+    if (Number.isNaN(d.getTime())) return false; // Invalid date
+    return d.toISOString().slice(0, 10) === dateString;
 }
 
 var userModel = require('./models/user');
@@ -42,39 +46,70 @@ var exerciseModel = require('./models/exercise');
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function(request, response) {
-  response.render(__dirname + '/views/index',{title:"Shorten URL",message:"Enter url"});
+    response.render(__dirname + '/views/index', {
+        title: "Shorten URL",
+        message: "Enter url"
+    });
 });
 
-app.post('/api/exercise/new-user', function(request,response)
-{
-  var original_url = request.params.new;
+app.post('/api/exercise/new-user', function(request, response) {
+    var original_url = request.params.new;
 
-  
-   if (/((ftp|http|https):\/\/)?(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/.test(request.params.input_url)){
-     
-   
-      var shortenUrl = new userModel({
-    originalUrl :request.params.input_url
+    if (/((ftp|http|https):\/\/)?(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/.test(request.params.input_url)) {
+        var user = new userModel({
+            username: request.body.username
+        });
+        user.save()
+            .then(doc => {
+                console.log(doc)
+                response.send({
+                    "user": doc
+                });
+            })
+            .catch(err => {
+                response.send({
+                    "save database error": err
+                });
+            })
+        //response.send( {"error":"accept"});
+    } else {
+        response.send({
+            "error": "invalid URL"
+        });
+    }
 
 });
-    shortenUrl.save()
-   .then(doc => {
-     console.log(doc)
-      response.send( {"original_url":request.params.input_url,"short_url":doc._id});
-   })
-   .catch(err => {
-      response.send( {"save database error":err});
-   })
-     //response.send( {"error":"accept"});
-  }else
-  {
-     response.send( {"error":"invalid URL"});
-  }
-  
+
+app.post('/api/exercise/add', function(request, response) {
+    userModel.findOne({
+            _id: request.body.userId // search query
+        }).then(doc => {
+
+            var exercise = new exerciseModel({
+                userId: request.body.userId,
+                date: request.body.date,
+                description: request.body.description,
+                duration: request.body.duration
+            });
+            exercise.save().then(doc => {
+                response.send({
+                    "user": doc
+                });
+            }).catch(err => {
+                response.send({
+                    "error": err
+                });
+            });
+        })
+        .catch(err => {
+            response.send({
+                "error": err
+            });
+        });
+
 });
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function() {
-  console.log('Your app is listening on port ' + listener.address().port);
+    console.log('Your app is listening on port ' + listener.address().port);
 });
-
